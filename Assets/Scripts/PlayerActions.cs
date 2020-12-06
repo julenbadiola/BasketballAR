210b0b4e01659d1dccbf534e469a1715898ac462
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2020-12-06 16:29:54 +0100

    Throw script

---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    public GameObject ball;
+    public Transform cam;
+    public float ballDistance = 2f;
+    public float ballForce = 250f;
+    bool holdingBall = true;
+    Rigidbody ballRB;
+    
+    void Start()
+    {
+        ballRB = ball.GetComponent<Rigidbody>();
+        ballRB.useGravity = false;
+    }
+
+    void Update()
+    {
+        if(holdingBall == true){
+            if(Input.GetMouseButtonDown(0)){
+                holdingBall = false;
+                ballRB.useGravity = true;
+                ballRB.AddForce(cam.forward * ballForce);
+            }
+        }
+        
+    }
+
+    private void LateUpdate(){
+        if(holdingBall == true){
+            ball.transform.position = cam.position + cam.forward * ballDistance;
+
+        }
+    }
+}
