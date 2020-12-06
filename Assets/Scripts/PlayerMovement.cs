82c6854d029f42e09d9f83b7695831589dcfcebe
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2020-12-06 15:24:04 +0100

    Add jump force

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,12 +10,34 @@ public class PlayerMovement : MonoBehaviour
     float z;
     Vector3 move;
 
+    //Gravedad
+    Vector3 velocity;
+    public float gravity = -15f;
+
+    public Transform groundCheck;
+    float radio = 0.4f;
+    public LayerMask mask;
+    bool isGrounded = false;
+    public float jumpForce = 1f;
+
+    
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, radio, mask);
+        
+        if(isGrounded && velocity.y < 0){
+            velocity.y = gravity;
+        }
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
 
         move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
