6b97136646ac818c2829c4fa956a2110ac117bc4
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2020-12-06 15:12:52 +0100

    First person controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    float x;
+    float z;
+    Vector3 move;
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
