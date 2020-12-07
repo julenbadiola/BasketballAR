40d53f452e90a4a5e80756c3c40aa791b2e56c6c
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2020-12-07 18:00:06 +0100

    Some changes

---
+++ Assets/Scripts/ScoreArea.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour
+{
+    public ParticleSystem winEffect;
+
+    private void OnTriggerEnter(Collider coll){
+        if(coll.CompareTag("Ball")){
+            Debug.Log("CANASTA");
+            winEffect.Play();
+        }
+    }
+}
