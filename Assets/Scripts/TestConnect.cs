b7c4de4e40b6c2ae769fe51b7601dcec3f66978a
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2020-12-07 21:41:18 +0100

    Photon multiplayer approach

---
+++ Assets/Scripts/TestConnect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        print("Connecting to server.");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster (){
+        print("Connected to server.");
+        print(PhotonNetwork.LocalPlayer.NickName);
+    }
+
+    public override void OnDisconnected(Photon.Realtime.DisconnectCause cause){
+        print("Disconnected from server. Cause: " + cause.ToString());
+    }
+}
