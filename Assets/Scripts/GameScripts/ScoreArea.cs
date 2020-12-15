be73e20080206b378a12f897259b390d7c6ca7aa
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2020-12-15 12:34:57 +0100

    Win effect color of local player

--- Assets/Scripts/GameScripts/ScoreArea.cs
+++ Assets/Scripts/GameScripts/ScoreArea.cs
@@ -1,15 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
 
 public class ScoreArea : MonoBehaviour
 {
     public ParticleSystem winEffect;
 
+    void Start(){
+        if(PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("Color")){
+            int res = (int) PhotonNetwork.LocalPlayer.CustomProperties["Color"];
+            winEffect.startColor = MasterManager.getColorByIndex(res);
+        }
+    }
     private void OnTriggerEnter(Collider coll){
         if(coll.CompareTag("Ball")){
             Debug.Log("CANASTA");
             winEffect.Play();
         }
+        /*
+        else if(coll.CompareTag("OponentBall"))
+        {
+            Debug.Log("CANASTA ADVERSARIO");
+            winEffect.startColor = new Color(1, 0, 1, .5f);
+        }*/
     }
 }
